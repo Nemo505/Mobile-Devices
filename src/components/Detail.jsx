1a3b8fc7576b440cb4f6f8d6606f8b779a1719e7
nemo <dahliahalesia8@gmail.com>
2023-03-01 02:57:40 +0630

    detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -9,23 +9,22 @@ const Detail = () => {
     <div>
       {
         products.products.map((product)=> {
-          return(<div key={product.id} >
-              {(() => {
-                if (parseInt( location.state.productId)) { 
-                  return (
-                    <div className='bg-gray-500 flex' style={{height:'83vh'}}>
+          return( product.id == location.state.productId ?
+            <div key={product.id} >
+              
+              <div className='bg-gray-500 flex' style={{height:'83vh'}}>
                       <div className='bg-white m-auto p-10 text-center shadow-xl'>
               
-                        <div className='flex items-center gap-2 mx-auto'>
+                        <div className='flex items-center mr-0 gap-2 mx-auto'>
                             {/* {location.state.productId} */}
-                            <div className='flex-1 text-end'>
-                              <img src="../../src/assets/shiroe.jpg" alt="" className='object-contain h-30 w-30 md:h-58 md:w-96'/>
+                            <div className='flex-1 mr-0'>
+                              <img src={product.images[0]} alt="" className='object-contain h-30 w-30 md:h-58 md:w-96'/>
                             </div>
                             <div className='flex-1 text-start'>
-                                <h3 className='font-semibold font-serif'>name</h3>
+                                <h3 className='font-semibold font-serif'>{product.title}</h3>
                                 <hr className='my-5 w-10 h-1 bg-red-500' />
-                                <p className='font-mono leading-loose'>dfafhelfneke;e</p>
-                                <h3 className='my-3 tracking-wideest font-semibold text-red-600'>$300</h3>
+                                <p className='font-mono leading-loose'>{product.description}</p>
+                                <h3 className='my-3 tracking-wideest font-sans font-semibold text-red-600'>${product.price}</h3>
               
                                 <label htmlFor="cars">Stock</label>
                                 <div className='md:flex gap-3'>
@@ -44,12 +43,8 @@ const Detail = () => {
                             
                         </div>
                       </div>
-                    </div>
-                  )
-                } 
-                })
-              }
-          </div>
+              </div>
+          </div> : null
             
           )
         })
