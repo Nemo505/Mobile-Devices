5f4b8f0c4e9eeb8da180a72a1c1f4fbb196bba38
nemo <dahliahalesia8@gmail.com>
2023-03-06 00:06:24 +0630

    responsive

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -19,40 +19,49 @@ const Detail = () => {
           return( product.id == location.state.productId ?
             <div key={product.id} >
               
-              <div className='bg-gray-500 flex ' style={{height:'84vh'}}>
-                      <div className='bg-white m-auto p-10 text-center shadow-xl'>
-              
-                        <div className='flex items-center mr-0 gap-2 mx-auto'>
-                            {/* {location.state.productId} */}
-                            <div className='flex-1 mr-0'>
-                              <img src={product.images[0]} alt="" className='object-contain h-30 w-30 md:h-58 md:w-96'/>
+              <div className='bg-gray-500 flex items-center justify-center ' style={{height: "85vh"}}>
+                  <div className='grid md:grid-cols-2 gap-4 py-2 bg-white md:h-4/5'>
+    
+                    <div className='col-span-1 m-auto md:px-2'>
+                        {/* {location.state.productId} */}
+                          <img src={product.images[0]} alt="" className='object-contain rounded-md h-28 md:h-58 md:w-96 mx-auto hover:saturate-150'/>
+                          <div  className='mt-4 flex flex-wrap gap-3'>
+                              {product.images.map(image => {
+                                return <img src={image} alt="" className='object-contain rounded h-4 md:h-14 mx-auto hover:border hover:border-red-600'/>
+                              })
+                            }
+                          </div>
+                        
+                    </div>
+
+                    <div className='col-span-1 text-start my-auto mx-10'>
+                      <div className=''>
+
+                        <h3 className='font-semibold font-serif'>{product.title}</h3>
+                        <hr className='my-5 w-10 h-1 bg-red-500' />
+                        <p className='font-mono leading-loose'>{product.description}</p>
+                        <h3 className='my-3 tracking-wideest font-sans font-semibold text-red-600'>${product.price}</h3>
+      
+                        <label htmlFor="cars">Stock</label>
+                        <div className='md:flex gap-3'>
+                          <div className='flex gap-3 items-center'>
+                            <button onClick={minus}>
+                              -
+                            </button>
+                            <div className='px-5 bg-slate-200 border border-red-500 rounded-md'>
+                                {count}
                             </div>
-                            <div className='flex-1 text-start'>
-                                <h3 className='font-semibold font-serif'>{product.title}</h3>
-                                <hr className='my-5 w-10 h-1 bg-red-500' />
-                                <p className='font-mono leading-loose'>{product.description}</p>
-                                <h3 className='my-3 tracking-wideest font-sans font-semibold text-red-600'>${product.price}</h3>
-              
-                                <label htmlFor="cars">Stock</label>
-                                <div className='md:flex gap-3'>
-                                  <div className='flex gap-3 items-center'>
-                                    <button onClick={minus}>
-                                      -
-                                    </button>
-                                    <div className='px-5 bg-slate-200 border border-red-500 rounded-md'>
-                                        {count}
-                                    </div>
-                                    <button onClick={() => setCount(count => count < product.stock ? count+1: count)}>
-                                      +
-                                    </button>
-                                  </div>
-                                  <button className='border px-7 py-1 my-2 rounded-md text-white bg-red-500'> Add to Cart</button>
-                                </div>
-              
-                            </div>
-                            
+                            <button onClick={() => setCount(count => count < product.stock ? count+1: count)}>
+                              +
+                            </button>
+                          </div>
+                          <button className='border px-7 py-1 my-2 rounded-md text-white bg-red-500'> Add to Cart</button>
                         </div>
                       </div>
+      
+                    </div>
+
+                  </div>
               </div>
           </div> : null
             
